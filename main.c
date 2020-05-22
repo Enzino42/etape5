48263ffb7afe3e26095c0f7814637ea9a0391492
Enziño <44849985+Enzino42@users.noreply.github.com>
2020-05-22 11:31:20 +0200

    Update main.c

--- main.c
+++ main.c
@@ -9,7 +9,8 @@ const double v0=0;
 const double alpha=0;
 const double xi0=10.0,yi0=10.0;
 int i=0;
-double x,y;
+
+
 
 void delay_100ms(){
     clock_t t0, t1 ;
@@ -27,16 +28,18 @@ double trajectoire_X(double t){
 
 double trajectoire_Y(double t){
     double y;
-    y=(-1/2)*g*pow(t,2)+v0*sin(alpha)*t+yi0;
+    y=(-0.5)*g*pow(t,2)+v0*sin(alpha)*t+yi0;
     return y;
 }
 
 int main(){
+    double t=0,x,y;
     for(i=0;i<10;i++){
-        trajectoire_X(0);
-        trajectoire_Y(0);
-        printf("x : %f ",&x);
-        printf("y : %f \n",&y);
+        x=trajectoire_X(t);
+        y=trajectoire_Y(t);
+        printf("x : %f ",x);
+        printf("y : %f \n",y);
+        t=t+0.1;
         delay_100ms();
     }
 }
