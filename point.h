681da66d74f6e4dc7b3e71c70de8c6b0308a4a56
Enziño <44849985+Enzino42@users.noreply.github.com>
2020-05-22 11:26:26 +0200

    Update point.h

--- point.h
+++ point.h
@@ -1,1 +1,20 @@
+#ifndef POINT_H_INCLUDED
+#define POINT_H_INCLUDED
 
+
+typedef struct {
+    double xi0,yi0,alpha,v0,ym,xm,ytraj,xtraj;
+    int xpx,ypx;
+} point ;
+
+//fonction :
+extern point XYmTopx(point);//
+extern point XYpxTom(point);// convertisseurs
+extern void print_point(point);// affiche les parametre d'un point
+extern point init_point(point,double,double);//initialise un pointavec les valeur math�matique
+extern point placer_pointX(point, double); //place un point a la coordon� xm
+extern point placer_pointY(point,double); //place un point a la coordon� ym
+extern point placer_pointXY(point,double,double);//place un point coordoner xm ym
+extern point deplacer_pointXY(point,double,double);//depalce un point de dx dy
+
+#endif
